33ab8b5c4f46563352d8341e10cef36d50cddf45
x_wang <18912540921@163.com>
2019-07-16 10:48:34 +0800

    basic layouts view

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>关于页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('content')
+  <h1>关于页1</h1>
+@stop
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>帮助页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('content')
+  <h1>帮助页1</h1>
+@stop
