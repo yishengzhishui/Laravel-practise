3eda1a0617873bbc5cbc12fa3568e769ddb85a30
x_wang <18912540921@163.com>
2019-07-17 12:25:38 +0800

    add show view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function create(){
         return view('users.create');
+    }
+
+    public function show(User $user){
+        // $user can be used in view
+        return view('users.show', compact('user'));
     }
 }
