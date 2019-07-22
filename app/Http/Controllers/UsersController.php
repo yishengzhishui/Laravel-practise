2510439317595f6094c545d235237eafd1f09f2c
x_wang <18912540921@163.com>
2019-07-22 10:35:27 +0800

    user only edit himself

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -44,11 +44,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
